0f2a509c5d88aac43abad5dd67efbce127ebc07e
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-08-31 18:29:02 -0300

    :tada: init: Projeto Criado

---
+++ src/main/java/com/fernandocanabarro/desafio_bossabox_tools/DesafioBossaboxToolsApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_bossabox_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBossaboxToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBossaboxToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/desafio_bossabox_tools/DesafioBossaboxToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_bossabox_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBossaboxToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
